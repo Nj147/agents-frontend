e9668a301349ee70946c30326bc7f84da137b353
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2021-07-01 09:47:11 +0100

    completed test for submit client code method

--- test/uk/gov/hmrc/agentsfrontend/controllers/InputClientCodeControllerSpec.scala
+++ test/uk/gov/hmrc/agentsfrontend/controllers/InputClientCodeControllerSpec.scala
@@ -16,14 +16,25 @@
 
 package uk.gov.hmrc.agentsfrontend.controllers
 
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{mock, when}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
 import play.api.http.Status
-import play.api.test.FakeRequest
-import play.api.test.Helpers._
+import play.api.test.{FakeRequest, Helpers}
+import play.api.test.Helpers.{contentType, defaultAwaitTimeout, status}
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
+import uk.gov.hmrc.agentsfrontend.connectors.InputClientCodeConnector
+import uk.gov.hmrc.agentsfrontend.persistence.domain.AgentClient
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import uk.gov.hmrc.agentsfrontend.services.InputClientCodeService
+import uk.gov.hmrc.agentsfrontend.views.html.InputClientCode
+
+import scala.concurrent.Future
 
 class InputClientCodeControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
   override def fakeApplication(): Application =
@@ -35,18 +46,40 @@ class InputClientCodeControllerSpec extends AnyWordSpec with Matchers with Guice
       .build()
 
   private val fakeRequest = FakeRequest("GET", "/")
+  val clientCode: InputClientCode = app.injector.instanceOf[InputClientCode]
+  val service: InputClientCodeService = mock(classOf[InputClientCodeService])
+  val controller = new InputClientCodeController(Helpers.stubMessagesControllerComponents(), clientCode, service)
+  val obj: AgentClient = AgentClient("agent", "client")
 
-  private val controller = app.injector.instanceOf[InputClientCodeController]
-
-  "GET /" should {
+  "GET /clientCode" should {
     "return 200" in {
-      val result = controller.inputClientCode(fakeRequest)
+      val result = controller.getInputClientCode(fakeRequest)
       status(result) shouldBe Status.OK
     }
     "return HTML" in {
-      val result = controller.inputClientCode(fakeRequest)
+      val result = controller.getInputClientCode(fakeRequest)
       contentType(result) shouldBe Some("text/html")
-      charset(result)     shouldBe Some("utf-8")
+      Helpers.charset(result)     shouldBe Some("utf-8")
+    }
+  }
+
+  "submitClientCode" should {
+    "return 204 when successfully added agent to client and redirected to success page" in {
+      when(service.postClientCode(any())) thenReturn(Future.successful(204))
+//      val result = controller.submitClientCode(FakeRequest("POST", "/clientCode"))
+      val result = controller.submitClientCode().apply(FakeRequest("POST", "/clientCode").withSession("arn" -> "agent").withFormUrlEncodedBody("crn" -> "client"))
+//        .withHeaders("Content-Type" -> "application/json").withBody(Json.toJson(obj)))
+      status(result) shouldBe Status.SEE_OTHER
+    }
+    "return 404 when the client code provided does not exist in the client database" in {
+      when(service.postClientCode(any())) thenReturn(Future.successful(404))
+      val result = controller.submitClientCode().apply(FakeRequest("POST", "/clientCode").withSession("arn" -> "agent").withFormUrlEncodedBody("crn" -> "client"))
+      status(result) shouldBe Status.NOT_FOUND
+    }
+    "return 409 when the client code provided already has an agent linked with them" in {
+      when(service.postClientCode(any())) thenReturn(Future.successful(409))
+      val result = controller.submitClientCode().apply(FakeRequest("POST", "/clientCode").withSession("arn" -> "agent").withFormUrlEncodedBody("crn" -> "client"))
+      status(result) shouldBe Status.CONFLICT
     }
   }
 }
