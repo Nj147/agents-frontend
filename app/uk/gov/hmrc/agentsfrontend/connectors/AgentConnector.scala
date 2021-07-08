538fce7a8c364751946a7075a6af99afd7042857
“QAcpardeep” <cpardeep@qa.com>
2021-07-08 13:58:12 +0100

    tests fixed

--- app/uk/gov/hmrc/agentsfrontend/connectors/AgentConnector.scala
+++ app/uk/gov/hmrc/agentsfrontend/connectors/AgentConnector.scala
@@ -17,7 +17,6 @@
 package uk.gov.hmrc.agentsfrontend.connectors
 
 import play.api.libs.json.Json
-
 import javax.inject.Inject
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
